7031d8b0dcbb31632c07358a93aa173170e74d84
llw2 <llw2.njit.edu>
2022-09-22 07:25:28 -0400

    added error checking for database setup

--- lib/config.php
+++ lib/config.php
@@ -1,6 +1,11 @@
 <?php
 
 $ini = @parse_ini_file(".env");
+if (!$ini) {
+    # missing the .env file
+    if ($_SERVER['SERVER_NAME'] == "localhost")
+        echo "Warning Missing Database Environment Variables<br>";
+}
 
 if($ini && isset($ini["DB_URL"])){
     //load local .env file
@@ -8,10 +13,9 @@ if($ini && isset($ini["DB_URL"])){
     $db_url = parse_url($url);
 }
 else{
-    //load from heroku env variables
+    //load from heroku env variables  should not get here.
     $url = getenv("DB_URL");
-    $db_url = parse_url($url);
-    
+    $db_url = parse_url($url);  
 }
 //attempts to handle a failure where parse_url doesn't parse properly (usually happens when special characters are included)
 if (!$db_url || count($db_url) === 0) {
@@ -23,8 +27,14 @@ if (!$db_url || count($db_url) === 0) {
     $db_url["pass"] = $matches[2];
     $db_url["path"] = "/" . $matches[5];
 }
-$dbhost   = $db_url["host"];
-$dbuser = $db_url["user"];
-$dbpass = $db_url["pass"];
-$dbdatabase       = substr($db_url["path"],1);
+
+if (count($db_url) === 1) {
+    echo "SETUP ERROR: Missing Database Setup Information. Contact developer of this app. <br>";
+    exit();
+} else {
+    $dbhost   = $db_url["host"];
+    $dbuser = $db_url["user"];
+    $dbpass = $db_url["pass"];
+    $dbdatabase       = substr($db_url["path"],1);
+}
 ?>
