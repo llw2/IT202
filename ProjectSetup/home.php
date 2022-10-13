b491f07e0490da4ca08329eb0d02b9a07ca4da73
llw2 <43096524+llw2@users.noreply.github.com>
2022-10-13 16:54:18 -0400

    Update home.php

--- ProjectSetup/home.php
+++ ProjectSetup/home.php
@@ -5,11 +5,11 @@ require(__DIR__ . "/../partials/nav.php");
 <?php
 //TODO 1  replace the following 2 lines with the current if (is_logged()) { and echo "Welcome statement
 /*
-if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
-    echo "Welcome, " . $_SESSION["user"]["email"]; 
-*/
 if (is_logged_in()) {
     echo "Welcome, " . get_user_email();
+*/
+if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+    echo "Welcome, " . $_SESSION["user"]["email"]; 
 } else {
     echo "You're not logged in";
 }
