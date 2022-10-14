a5f15c57740ebeeadfefb249febca783473e267e
llw2 <43096524+llw2@users.noreply.github.com>
2022-10-14 13:07:42 -0400

    Update nav.php

--- ProjectSetup/nav.php
+++ ProjectSetup/nav.php
@@ -34,4 +34,19 @@ require(__DIR__ . "/../lib/functions.php");
         <li><a href="register.php">Register</a></li>
         <li><a href="logout.php">Logout</a></li>
     </ul>
+    //TODO 2
+    /*
+    <ul>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="home.php">Home</a></li>
+        <?php endif; ?>
+        <?php if (!is_logged_in()) : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
+    </ul>
+    */
 </nav>
