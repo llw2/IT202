93fb711359c725d4a690d53af1990d6d11e9e28d
llw2 <llw2.njit.edu>
2022-10-19 14:29:14 -0400

    reformated TODO2 comments in nav.php

--- ProjectSetup/nav.php
+++ ProjectSetup/nav.php
@@ -31,23 +31,15 @@ require(__DIR__ . "/../lib/functions.php");
 ?>
 <nav>
     <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    </ul>
-    <!-- TODO 2
-    <ul>
-        <?php if (is_logged_in()) : ?>
+//TODO 2        <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
-        <?php endif; ?>
-        <?php if (!is_logged_in()) : ?>
+//TODO 2        <?php endif; ?>
+//TODO 2        <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
-        <?php endif; ?>
-        <?php if (is_logged_in()) : ?>
+//TODO 2        <?php endif; ?>
+//TODO 2        <?php if (is_logged_in()) : ?>
             <li><a href="logout.php">Logout</a></li>
-        <?php endif; ?>
-    </ul>
-    -->
+//TODO 2        <?php endif; ?>
+    </ul> 
 </nav>
