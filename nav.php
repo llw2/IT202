2fe46e9968fb9d56abd94088931cc38e44b69fb2
llw2 <llw2.njit.edu>
2022-11-30 07:52:23 -0500

    modified nav.php to work on njit servers

--- nav.php
+++ nav.php
@@ -1,15 +1,7 @@
 <?php
-require_once(__DIR__ . "/../lib/functions.php");
-//Note: this is to resolve cookie issues with port numbers
-$domain = $_SERVER["HTTP_HOST"];
-if (strpos($domain, ":")) {
-    $domain = explode(":", $domain)[0];
-}
-$localWorks = true; //some people have issues with localhost for the cookie params
-//if you're one of those people make this false
 
 //this is an extra condition added to "resolve" the localhost issue for the session cookie
-if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+if ($_SERVER["HTTP_HOST"] == "web.njit.edu") {
     session_set_cookie_params([
         "lifetime" => 60 * 60,
         "path" => "$BASE_PATH",
@@ -21,7 +13,7 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     ]);
 }
 session_start();
-
+require_once(__DIR__ . "/../lib/functions.php");
 
 ?>
 <!-- include css and js files -->
